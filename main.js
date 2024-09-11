817cb0cdcb2ae134862eee6955de68720b5ee1eb
Waleed Alhrbi <waleedalhrbi2001@gmail.com>
2024-09-11 11:53:37 +0300

    Creat Taske

---
+++ main.js
@@ -0,0 +1,42 @@
+let welcomeT = document.getElementsByClassName("welcomeText")[0];
+
+welcomeT.style.color = "white"
+welcomeT.style.background = "blue"
+
+let prgraphMide = document.getElementById("prgraphMid");
+prgraphMide.style.fontWeight = "bold";
+
+prgraphMide.classList.add("prgraphMids");
+
+let imgFloer = document.getElementsByTagName("img")[0];
+
+imgFloer.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRopxhys1sDXRymAf4bLHhYOhhM123zapzmtw&s";
+
+
+
+
+    let ulist = document.querySelector('ul');
+    ulist
+    for (let i = 1; i <= 3; i++) {
+        let listItem = document.createElement("li");
+        listItem.textContent = "waleed";
+        ulist.appendChild(listItem);
+        listItem.style.display = "inline-block";
+        ulist.style.color = "blue";
+        ulist.style.border = "2px solid #0000FF";
+    }
+    
+    
+     
+
+let tableContai = document.getElementById("tableContainer");
+
+let table = Document.createElement("table");
+
+table.className = "tstyle";
+
+let row1 = table.insertRow();
+
+for(let i=0; i<=3; i++){
+    
+}
