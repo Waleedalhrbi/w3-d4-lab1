c2335591af85a5ca4b383d6900c0ee82bbcf38ce
Waleed Alhrbi <waleedalhrbi2001@gmail.com>
2024-09-11 12:23:10 +0300

    Creat table

--- main.js
+++ main.js
@@ -16,27 +16,35 @@ imgFloer.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRopxhys1sDXR
 
 
     let ulist = document.querySelector('ul');
-    ulist
+
     for (let i = 1; i <= 3; i++) {
         let listItem = document.createElement("li");
         listItem.textContent = "waleed";
         ulist.appendChild(listItem);
-        listItem.style.display = "inline-block";
-        ulist.style.color = "blue";
-        ulist.style.border = "2px solid #0000FF";
     }
+    ulist.classList.add("ulistt");
     
+
+    let table = document.createElement("table");
+    table.className = "styleTab";
     
-     
+    let row1 = table.insertRow();
+    let cell1 = row1.insertCell(0);
+    let cell2 = row1.insertCell(1);
+    let cell3 = row1.insertCell(2);
+    cell1.textContent = "Day1";
+    cell2.textContent = "Day2";
+    cell3.textContent = "Day3";
 
-let tableContai = document.getElementById("tableContainer");
 
-let table = Document.createElement("table");
+let row2 = table.insertRow();
+let cell4 = row2.insertCell(0);
+let cell5 = row2.insertCell(1);
+let cell6 = row2.insertCell(2);
+cell4.textContent = "Sunday";
+cell5.textContent = "Moonday";
+cell6.textContent = "Tuesday";
 
-table.className = "tstyle";
+document.getElementById("tableCont").appendChild(table);
 
-let row1 = table.insertRow();
 
-for(let i=0; i<=3; i++){
-    
-}
